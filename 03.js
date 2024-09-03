7042b201bd64a1c61779ac58b16af015a806a895
Pietro Bucker <pietro_177@hotmail.com>
2024-09-03 20:26:46 -0300

    Fix: Troca do arquivo json mockado pelo arquivo fornecido pelo teste

--- 03.js
+++ 03.js
@@ -1,18 +1,18 @@
-const { FaturamentoDiario } = require('./03.json');
+const FaturamentoDiario = require('./03.json');
 
 function faturamento(FaturamentoDiario) {
-    const arrayFaturamento = FaturamentoDiario.filter(item => item.faturamento > 0)
-        .map(item => item.faturamento);
+    const arrayFaturamento = FaturamentoDiario.filter(item => item.valor > 0)
+        .map(item => item.valor);
     const mediaFaturamento = arrayFaturamento.reduce((acc, item) => acc + item, 0) / arrayFaturamento.length;
 
     const maiorFaturamento = Math.max(...arrayFaturamento);
     const menorFaturamento = Math.min(...arrayFaturamento);
 
     const qtdDiasMelhoFaturamento = arrayFaturamento.filter(item => item > mediaFaturamento);
-
+    
     return `
-    Maior faturamento: ${maiorFaturamento}
-    Menor faturamento: ${menorFaturamento}
+    Maior faturamento: ${maiorFaturamento} no dia ${FaturamentoDiario.find(item => item.valor === maiorFaturamento).dia}
+    Menor faturamento: ${menorFaturamento} no dia ${FaturamentoDiario.find(item => item.valor === menorFaturamento).dia}
     Média faturamento: ${mediaFaturamento}
     Dias com faturamento acima da média: ${qtdDiasMelhoFaturamento.length}`;
 }
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,10 +1,20 @@
-let indice = 13;
-let k = 0;
-let soma = 0;
+const FaturamentoDiario = require('./03.json');
 
-while (k < indice) {
-    soma += k
-    k += 1
+function faturamento(FaturamentoDiario) {
+    const arrayFaturamento = FaturamentoDiario.filter(item => item.valor > 0)
+        .map(item => item.valor);
+    const mediaFaturamento = arrayFaturamento.reduce((acc, item) => acc + item, 0) / arrayFaturamento.length;
+
+    const maiorFaturamento = Math.max(...arrayFaturamento);
+    const menorFaturamento = Math.min(...arrayFaturamento);
+
+    const qtdDiasMelhoFaturamento = arrayFaturamento.filter(item => item > mediaFaturamento);
+    
+    return `
+    Maior faturamento: ${maiorFaturamento} no dia ${FaturamentoDiario.find(item => item.valor === maiorFaturamento).dia}
+    Menor faturamento: ${menorFaturamento} no dia ${FaturamentoDiario.find(item => item.valor === menorFaturamento).dia}
+    Média faturamento: ${mediaFaturamento}
+    Dias com faturamento acima da média: ${qtdDiasMelhoFaturamento.length}`;
 }
 
-console.log(soma) // Resultado da soma é 91
+console.log(faturamento(FaturamentoDiario));
